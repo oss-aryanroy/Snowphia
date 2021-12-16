5e23f1e9b7cb52c3e42f9c544369bea4bf082bd8
wsssss <=>
2021-12-17 00:23:36 +0530

    adding gitpull command

--- cogs/fun.py
+++ cogs/fun.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from utils.cmds import VishAPI
 from typing import Union, Optional
 
+
 class Fun(commands.Cog):
     def __init__(self, bot) -> None:
         self.bot = bot
@@ -13,7 +14,7 @@ class Fun(commands.Cog):
         avatar = member.avatar.url if member.avatar else member.default_avatar.url
         vish = VishAPI(self.bot, member)
         embed, file = await vish.get_embed(ctx.command.name, url=avatar)
-        await ctx.reply(embed=embed, file=file,mention_author=False)
+        await ctx.reply(embed=embed, file=file, mention_author=False)
 
     @commands.command()
     async def invert(self, ctx: commands.Context, member: Union[discord.Member, discord.User] = None):
@@ -21,7 +22,7 @@ class Fun(commands.Cog):
         avatar = member.avatar.url if member.avatar else member.default_avatar.url
         vish = VishAPI(self.bot, member)
         embed, file = await vish.get_embed(ctx.command.name, url=avatar)
-        await ctx.reply(embed=embed, file=file,mention_author=False)
+        await ctx.reply(embed=embed, file=file, mention_author=False)
 
     @commands.command()
     async def blur(self, ctx: commands.Context, member: Union[discord.Member, discord.User] = None):
@@ -29,7 +30,7 @@ class Fun(commands.Cog):
         avatar = member.avatar.url if member.avatar else member.default_avatar.url
         vish = VishAPI(self.bot, member)
         embed, file = await vish.get_embed(ctx.command.name, url=avatar)
-        await ctx.reply(embed=embed, file=file,mention_author=False)
+        await ctx.reply(embed=embed, file=file, mention_author=False)
 
     @commands.command(aliases=['colors'])
     async def primary(self, ctx: commands.Context, member: Union[discord.Member, discord.User] = None):
@@ -37,7 +38,7 @@ class Fun(commands.Cog):
         avatar = member.avatar.url if member.avatar else member.default_avatar.url
         vish = VishAPI(self.bot, member)
         embed, file = await vish.get_embed(ctx.command.name, url=avatar)
-        await ctx.reply(embed=embed, file=file,mention_author=False)
+        await ctx.reply(embed=embed, file=file, mention_author=False)
 
     @commands.command(aliases=['embo'])
     async def emboss(self, ctx: commands.Context, member: Union[discord.Member, discord.User] = None):
@@ -45,7 +46,7 @@ class Fun(commands.Cog):
         avatar = member.avatar.url if member.avatar else member.default_avatar.url
         vish = VishAPI(self.bot, member)
         embed, file = await vish.get_embed(ctx.command.name, url=avatar)
-        await ctx.reply(embed=embed, file=file,mention_author=False)
+        await ctx.reply(embed=embed, file=file, mention_author=False)
 
     @commands.command(aliases=['fg'])
     async def frostedglass(self, ctx: commands.Context, member: Union[discord.Member, discord.User] = None):
@@ -53,15 +54,15 @@ class Fun(commands.Cog):
         avatar = member.avatar.url if member.avatar else member.default_avatar.url
         vish = VishAPI(self.bot, member)
         embed, file = await vish.get_embed(ctx.command.name, url=avatar)
-        await ctx.reply(embed=embed, file=file,mention_author=False)
-        
+        await ctx.reply(embed=embed, file=file, mention_author=False)
+
     @commands.command()
     async def mirror(self, ctx: commands.Context, member: Union[discord.Member, discord.User] = None):
         member = member or ctx.author
         avatar = member.avatar.url if member.avatar else member.default_avatar.url
         vish = VishAPI(self.bot, member)
         embed, file = await vish.get_embed(ctx.command.name, url=avatar)
-        await ctx.reply(embed=embed, file=file,mention_author=False)
+        await ctx.reply(embed=embed, file=file, mention_author=False)
 
     @commands.command()
     async def lego(self, ctx: commands.Context, member: Union[discord.Member, discord.User] = None):
@@ -69,7 +70,7 @@ class Fun(commands.Cog):
         avatar = member.avatar.url if member.avatar else member.default_avatar.url
         vish = VishAPI(self.bot, member)
         embed, file = await vish.get_embed(ctx.command.name, url=avatar)
-        await ctx.reply(embed=embed, file=file,mention_author=False)
+        await ctx.reply(embed=embed, file=file, mention_author=False)
 
     @commands.command()
     async def oil(self, ctx: commands.Context, member: Union[discord.Member, discord.User] = None):
@@ -77,15 +78,15 @@ class Fun(commands.Cog):
         avatar = member.avatar.url if member.avatar else member.default_avatar.url
         vish = VishAPI(self.bot, member)
         embed, file = await vish.get_embed(ctx.command.name, url=avatar)
-        await ctx.reply(embed=embed, file=file,mention_author=False)
-    
+        await ctx.reply(embed=embed, file=file, mention_author=False)
+
     @commands.command()
     async def flip(self, ctx: commands.Context, member: Union[discord.Member, discord.User] = None):
         member = member or ctx.author
         avatar = member.avatar.url if member.avatar else member.default_avatar.url
         vish = VishAPI(self.bot, member)
         embed, file = await vish.get_embed(ctx.command.name, url=avatar)
-        await ctx.reply(embed=embed, file=file ,mention_author=False)
+        await ctx.reply(embed=embed, file=file, mention_author=False)
 
     @commands.command(aliases=['ht'])
     async def halftone(self, ctx: commands.Context, member: Union[discord.Member, discord.User] = None):
@@ -93,22 +94,27 @@ class Fun(commands.Cog):
         avatar = member.avatar.url if member.avatar else member.default_avatar.url
         vish = VishAPI(self.bot, member)
         embed, file = await vish.get_embed(ctx.command.name, url=avatar)
-        await ctx.reply(embed=embed, file=file ,mention_author=False)
+        await ctx.reply(embed=embed, file=file, mention_author=False)
 
     @commands.command(aliases=['gradient', 'grad'])
-    async def filter(self, ctx: commands.Context, member: Optional[Union[discord.Member, discord.User]] = None, gradient: str = "oceanic"):
-        possible = "dramatic, firenze, golden, lix, lofi, neue, obsidian, pastel_pink, ryo, oceanic, marine, seagreen, flagblue, liquid, diamante , radio, twenties, rosetint, mauve, bluechrome, vintage, perfume, serenity"
+    async def filter(self, ctx: commands.Context, member: Optional[Union[discord.Member, discord.User]] = None,
+                     gradient: str = "oceanic"):
+        possible = "dramatic, firenze, golden, lix, lofi, neue, obsidian, pastel_pink, ryo, oceanic, marine, " \
+                   "seagreen, flagblue, liquid, diamante , radio, twenties, rosetint, mauve, bluechrome, vintage, " \
+                   "perfume, serenity "
         new_possibles = possible.split(', ')
         if gradient == "list":
             actual = ', '.join([f"`{text}`" for text in possible.split(', ')])
             return await ctx.reply(f"Here are all the possible gradients: {actual}\n**Default:** `oceanic`")
         if gradient not in new_possibles:
-                return await ctx.send(f"An Inappropriate Gradient was passed, use `{ctx.prefix}gradient list` to check all gradients possible")
+            return await ctx.send(
+                f"An Inappropriate Gradient was passed, use `{ctx.prefix}gradient list` to check all gradients possible")
         member = member if isinstance(member, (discord.Member, discord.User)) else ctx.author or ctx.author
         avatar = member.avatar.url if member.avatar else member.default_avatar.url
         vish = VishAPI(self.bot, member)
         embed, file = await vish.get_embed(ctx.command.name, url=avatar, filter=gradient.lower())
-        await ctx.reply(embed=embed, file=file ,mention_author=False)
+        await ctx.reply(embed=embed, file=file, mention_author=False)
+
 
 def setup(bot):
     bot.add_cog(Fun(bot))
