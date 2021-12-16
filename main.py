1529e61c3ba0e9543cacd7a34262397ff3b2d756
wsssss <=>
2021-12-17 04:26:36 +0530

    adding gitpull command

--- main.py
+++ main.py
@@ -105,9 +105,9 @@ async def gitpull(ctx: commands.Context):
     message = await ctx.send('Pulling latest files from repo...')
     repo = git.Repo('/home/ubuntu/Snowphia/.git')
     ret = repo.remotes.origin.pull()
+    await message.delete()
     if ret[0].flags == 4:
         return
-    await message.delete()
     command = client.get_command('restart')
     await ctx.invoke(command)
 
