4c9923e51e6630fbe98b1df359fb29c0fe4d6e20
wsssss <=>
2021-12-18 00:55:30 +0530

    adding gitpull command

--- cogs/help.py
+++ cogs/help.py
@@ -5,3 +5,12 @@ from discord.ext import commands
 class HelpClass(commands.HelpCommand):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
+
+
+class HelpCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+def setup(bot):
+    bot.add_cog(HelpCog(bot))
