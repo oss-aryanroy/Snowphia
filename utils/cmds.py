b637042e52f74e3bd9f782ac05a6de5c5c93ad06
wsssss <=>
2021-12-17 15:39:46 +0530

    adding gitpull command

--- utils/cmds.py
+++ utils/cmds.py
@@ -256,17 +256,13 @@ async def get_graph(bot, *args):
 @executor()
 def get_picture(image: BytesIO):
     img = Image.open(image)
-    display(img)
-
     height, width = img.size
     lum_img = Image.new('L', (height, width), 0)
-
     draw = ImageDraw.Draw(lum_img)
     draw.pieslice(((0, 0), (height, width)), 0, 360,
                   fill=255, outline="white")
     img_arr = np.array(img)
     lum_img_arr = np.array(lum_img)
-    display(Image.fromarray(lum_img_arr))
     final_img_arr = np.dstack((img_arr, lum_img_arr))
     image = Image.fromarray(final_img_arr)
     buffer = BytesIO()
