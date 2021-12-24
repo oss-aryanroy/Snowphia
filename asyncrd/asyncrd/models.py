2a617acf1feb420f48b1858a9c56d458e850f8cb
FreezingRose <rika032002@protonmail.com>
2021-12-24 18:25:02 +0530

    s

---
+++ asyncrd/asyncrd/models.py
@@ -0,0 +1,20 @@
+from typing import Tuple
+
+class BasicProtocol():    
+    def __init__(self, *query : str):
+        self.query : Tuple[str] = query
+
+class Set(BasicProtocol):
+    command = 'SET'
+    def __init__(self, key: str, value: str):
+        super().__init__(key, value)
+            
+class Get(BasicProtocol):
+    command = 'GET'
+    def __init__(self, key: str):
+        super().__init__(key)
+
+class Delete(BasicProtocol):
+    commands = 'DELETE'
+    def __init__(self, *keys: str):
+        super().__init__(*keys)
